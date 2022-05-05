859803afd406c840a7c135291cba32a8a3978be7
RyanK5618 <flazwell13@gmail.com>
2022-05-05 02:21:43 +0000

    add bouncing box

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,26 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas,20,20);
+            view.addChild(circle);
+            circles.push(circle); 
+        }
 
         // TODO 3 / 8 : Call the drawCircle() function 
+         drawCircle();
+         drawCircle();
+         drawCircle();
+         drawCircle();
+         drawCircle();
+          for (var drawCircle = 0; drawCircle < 100; drawCircle++) {
 
-
+          }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,11 +51,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+           game.checkCirclePosition(circles[0])
+           game.checkCirclePosition(circles[1])
+           game.checkCirclePosition(circles[2])
+           game.checkCirclePosition(circles[3])
+           game.checkCirclePosition(circles[4])
             // TODO 9 : Iterate over the array
            
             
@@ -60,10 +79,21 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            
             // TODO 7 : YOUR CODE STARTS HERE //////////////////////
+    
+                
             
-
+            if ( circle.x < 0) {
+                circle.x = canvas.width;
+            }
+            if ( circle.y > canvas.height) {
+                circle.y = 0;
+            }
+            if ( circle.y < 0) {
+                circle.y = canvas.height;
+            }
+            
+            
 
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
         }
